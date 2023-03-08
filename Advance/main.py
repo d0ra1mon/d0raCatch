6910e46ccf166e617274843d11594281877a31ce
nouseramefounded <100087356+nouseramefounded@users.noreply.github.com>
2023-03-08 15:33:59 +0100

    Sould work only on Linux

--- Advance/main.py
+++ Advance/main.py
@@ -2,10 +2,18 @@ try:
     import pandas as pd
     import glob
     import getpass
+    import os
+    from datetime import *
+    import time
 except ImportError:
     import os
     print("Installing dependencies...")
     os.system("pip3 install pandas")
+
+today = date.today()
+now = datetime.now()
+current_time = now.strftime("%H")
+real_date = (str(today)+"_"+str(current_time))
 
 i = input("Do you want to merge different file and drop duplicates? (y/n)")
 if i == 'y':
@@ -19,8 +27,17 @@ if i == 'y':
     # Leggi i file CSV in un singolo DataFrame
     df = pd.concat((pd.read_csv(f) for f in all_files), ignore_index=True)
     # Salva il DataFrame in un file CSV
-    df.to_csv('/home/'+user+'/Desktop/d0raCatch/Advance/Done/Wifi_merged.csv', index=False)
+    df.to_csv("/home/"+user+"/Desktop/d0raCatch/Advance/Done/Wifi_merged_"+real_date+".csv", index=False)
     print("Files merged")
+    i1 = input("Do you want to drop duplicates? (y/n)")
+    if i1 == 'y':
+        os.system("cp /home/"+user+"/Desktop/d0raCatch/Advance/Done/Wifi_merged_"+real_date+".csv /home/"+user+"/Desktop/d0raCatch/Advance/ToClean/")
+        input_data = pd.read_csv("/home/"+user+"/Desktop/d0raCatch/Advance/Done/Wifi_merged_"+real_date+".csv")
+        filtered_data = input_data.drop_duplicates(subset=['MAC'])
+        filtered_data.to_csv("/home/"+user+"/Desktop/d0raCatch/Advance/Done/Cleaned_Wifi_merged_"+real_date, index=False)
+        print("File Wifi_merged_"+real_date+".csv cleaned")
+    else:
+        exit(1)
 else:
     print("Move all file to merge into ToClean folder then press ENTER")
     input()
@@ -28,5 +45,5 @@ else:
     user = getpass.getuser()
     input_data = pd.read_csv("/home/"+user+"/Desktop/d0raCatch/Advance/ToClean/"+file)
     filtered_data = input_data.drop_duplicates(subset=['MAC'])
-    filtered_data.to_csv("/home/"+user+"/Desktop/d0raCatch/Advance/Done/C_"+file, index=False)
-    print("Files cleaned saved on /home/"+user+"/Desktop/d0raCatch/Advance/Done/")
+    filtered_data.to_csv("/home/"+user+"/Desktop/d0raCatch/Advance/Done/Cleaned_"+file+"_"+real_date, index=False)
+    print("File cleaned saved on /home/"+user+"/Desktop/d0raCatch/Advance/Done/")
